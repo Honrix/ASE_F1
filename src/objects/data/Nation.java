192929c09ecf75d7f983b1731ea07112518fbc5a
henry <henry-schulz@hotmail.de>
2022-05-23 12:52:00 +0200

    Update Data

---
+++ src/objects/data/Nation.java
@@ -0,0 +1,26 @@
+package objects.data;
+
+public enum Nation {
+
+    AUS ("Australia"),
+    CAN ("Canada"),
+    ESP ("Spain"),
+    FIN ("Finland"),
+    FRA ("France"),
+    GBR ("Great Britain"),
+    GER ("Germany"),
+    ITA ("Italy"),
+    JPN ("Japan"),
+    MEX ("Mexico"),
+    MON ("Monaco"),
+    NED ("Netherlands"),
+    RUS ("Russia");
+
+    private String nationName;
+
+    Nation (String nationName){
+        this.nationName = nationName;
+    }
+
+
+}
